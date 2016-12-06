62b7b8ee5a288046bd14e867e45841b07a664fd3
Jeong Jin Wook <happygrammer.dev@gmail.com>
2016-12-06 12:25:08 +0900

    Update

---
+++ snippets/component.styles.1.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `<span>Hello</span>`,
+  styles: ['span { font-style: italic; font-size: 50px; }']
+})
+export class AppComponent { }
